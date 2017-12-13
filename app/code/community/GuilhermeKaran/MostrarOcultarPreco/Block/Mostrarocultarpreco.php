8b70ea4e1feee6d9cbcad52031312a408a0d9185
Guilherme Karan Barbosa <32599782+karanguilherme@users.noreply.github.com>
2017-12-12 22:20:43 -0200

    Add files via upload

---
+++ app/code/community/GuilhermeKaran/MostrarOcultarPreco/Block/Mostrarocultarpreco.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guilherme
+ * Date: 20/11/17
+ * Time: 08:46
+ */
+
+class GuilhermeKaran_MostrarOcultarPreco_Block extends Mage_Core_Block_Template
+{
+
+}
---
+++ app/code/community/GuilhermeKaran/MostrarOcultarPreco/Helper/Data.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guilherme
+ * Date: 20/11/17
+ * Time: 08:47
+ */
+
+class GuilhermeKaran_MostrarOcultarPreco_Helper_Data extends  Mage_Core_Helper_Abstract
+{
+
+}
